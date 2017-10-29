94c39a42bed23c45fa70a35ee8bad30137b022c5
Fredrik Fagerholm <fagerholm.fredrik@gmail.com>
2017-10-29 16:03:36 +0200

    more demos

--- demo.m
+++ demo.m
@@ -1,6 +1,9 @@
 import qr_algorithm.*
 load('data/multivariate_normal.mat');
 
+% function for plotting 3D vectors starting from the origin
+% v is vector with 3 components
+% c is the color of the vector
 plot_3d_vector = @(v, c) quiver3(0, 0, 0, v(1), v(2), v(3), c);
 
 figure;
@@ -20,23 +23,28 @@ xlabel('x-axis');
 ylabel('y-axis');
 zlabel('z-axis');
 
+% construct the data matrix
 A = [X', Y', Z'];
-% center the data for PCA 
+% normalize the data for PCA 
 A_c = bsxfun(@minus, A, mean(A))/sqrt(max(size(A)) - 1);
 
 [U, S, V] = qr_algorithm.svd_qr(A_c);
+% output maximum absolute difference between 
+% actual data and SVD representation
 fprintf('Error: %e \n', max(max(abs(A_c - U*S*V'))));
 
-
+% 2 first principal vectors
 v1 = V(:, 1);
 v2 = V(:, 2);
+% plot principal vectors
 plot_3d_vector(3*v1, 'red');
 plot_3d_vector(3*v2, 'red');
+% find and plot plane spanned by the vectors
 a = cross(v1, v2);
 [x, y] = meshgrid([-3 3], [-3 3]);
 z = -(a(1)*x + a(2)*y)/a(3);
 s = surf(x, y, z);
-alpha(s, 0.5)
+set(s, 'FaceColor', [0 0 0], 'FaceAlpha', 0.3, 'EdgeAlpha', 0);
 
 % Projection onto first two principal components
 figure;
--- demo_iris.m
+++ demo_iris.m
@@ -1,12 +1,17 @@
 import qr_algorithm.*
 load('data/fisher.mat');
 
+% function for plotting 3D vectors starting from the origin
+% v is vector with 3 components
+% c is the color of the vector
 plot_3d_vector = @(v, c) quiver3(0, 0, 0, v(1), v(2), v(3), c);
 
 % Type 0 is Setosa; type 1 is Verginica; and type 2 is Versicolor.
 A = [PW, PL, SW];
-% center the data for PCA 
-A_c = bsxfun(@minus, A, mean(A))/sqrt(max(size(A)) - 1);
+% Mean center the data  
+A_c = bsxfun(@minus, A, mean(A));
+% prepare data for PCA
+Y = A_c/sqrt(max(size(A)) - 1);
 
 x_max = max(A_c(:,1));
 x_min = min(A_c(:,1));
@@ -20,7 +25,7 @@ grid on;
 %axis([x_min x_max y_min y_max z_min z_max])
 
 % 3D scatter plot pf data
-sc3d = scatter3(A_c(:, 1), A_c(:, 2), A_c(:, 3), 30, Type, 'filled');
+sc3d = scatter3(Y(:, 1), Y(:, 2), Y(:, 3), 30, Type, 'filled');
 xlabel('Petal width');
 ylabel('Petal length');
 zlabel('Sepal width');
@@ -34,16 +39,23 @@ line([0,0], yl, [0,0], 'LineWidth', 1, 'Color', 'k');
 line([0,0], [0,0], zl, 'LineWidth', 1, 'Color', 'k');
 
 % SVD of data
-[U, S, V] = qr_algorithm.svd_qr(A_c);
-fprintf('Error: %e \n', max(max(abs(A_c - U*S*V'))));
+[U, S, PC] = qr_algorithm.svd_qr(Y);
+% output maximum absolute difference between 
+% actual data and SVD representation
+fprintf('Error: %e \n', max(max(abs(Y - U*S*PC'))));
+
+% Variances
+V = S .* S;
 
 axis tight;
 
-% Plane spanned by two first principal components
-v1 = V(:, 1);
-v2 = V(:, 2);
+% 2 first principal vectors
+v1 = PC(:, 1);
+v2 = PC(:, 2);
+% plot principal vectors
 plot_3d_vector(2*v1, 'red');
 plot_3d_vector(2*v2, 'red');
+% find and plot plane spanned by the vectors
 a = cross(v1, v2);
 [x, y] = meshgrid([x_min x_max], [y_min y_max]);
 z = -(a(1)*x + a(2)*y)/a(3);
@@ -53,9 +65,12 @@ set(s, 'FaceColor', [0 0 0], 'FaceAlpha', 0.3, 'EdgeAlpha', 0);
 
 % Projection onto first two principal components
 A_red = U(:, 1:2)*S(1:2, 1:2);
+%A_red = PC' * A_c';
 
 figure;
-scatter(A_red(:, 1), A_red(:, 2), 30, Type, 'filled');
+s = scatter(A_red(:, 1), A_red(:, 2), 30, Type, 'filled');
 xlabel('1st principal component');
 ylabel('2nd principal component');
 %legend('Setosa', 'Verginica', 'Versicolor');
+
+fprintf('Explained variance: %f \n Ratio: %f \n', V(1,1) + V(2,2), (V(1,1) + V(2,2))/trace(V));
